0b21b1a3b9150eb785d9eb883e57a97d5beaf8bb
divine chukwuma <divinechukwuma982@gmail.com>
2023-11-03 15:56:44 +0100

    i added to my javascript and my css

--- loader.js
+++ loader.js
@@ -8,20 +8,42 @@ spinElement.style.display = 'none'
 let progressBarElement = document.querySelector('#progress-bar')
 progressBarElement.style.display = 'none'
 
-const progressBarBeforeElement = document.querySelector('.progress-bar::before');
+const progressBar = document.querySelector('.progress-bar');
+
 
 // get input field tags
 const color = document.querySelector('#color')
 const loaderType = document.querySelector('#loader-type')
 const duration = document.querySelector('#duration')
-function updateProgress( progressValue){
-    progress.style.setProperty('--progress-value',progressValue);
-    if (progressValue >= 100){
-        progressBarBeforeElement.style.display = 'none'
+const progressaValue = document.querySelectorAll(' --progress-value')
+const conicGradientRule = `conic-gradient(black calc(var(--progress-value) * 1%), white 0)`;
+const radialGradientRule = '(closest-side, rgb(100, 0, 0) 79%, transparent 80% 100%)';
+
+function checkProgress() {
+    const progressValue = parseFloat(getComputedStyle(progressBar).getPropertyValue('--progress-value'));
+    
+    if (progressValue === 100) {
+        progressBarElement.style.display = 'none'
+      // You can perform any actions you need here.
+    } else {
+      // If it's not 100%, continue checking
+      setTimeout(checkProgress, 100); // Poll every 100 milliseconds
     }
-}
+  }
+  
+  
+  color.addEventListener('input', function() {
+    const selectedColor = color.value;
+    const conicGradientRule = `conic-gradient(${selectedColor} calc(var(--progress-value) * 1%), blue 0)`;
+    progressBarElement.style.background = conicGradientRule;
+  });
+  
+  
 
-
+  
+  
+  // Start checking the progress
+  
 document.querySelector('#select-loader').addEventListener("click",
     function (e) {
         e.preventDefault()
@@ -62,14 +84,15 @@ document.querySelector('#select-loader').addEventListener("click",
 
                 case 'progress':
                     progressBarElement.style.display = 'block'
-                    color.addEventListener('input' , function() {
-                        const endingColor = colorPicker.value;
-                        progressBar.style.setProperty('--ending-color', endingColor);
-                      });
-                      updateProgress(100);
+                    checkProgress();
+                    
+                
+                    
+                
+                    
 
                     break;
-                    
+
 
 
 
